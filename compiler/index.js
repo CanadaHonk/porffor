d1b8f2e39296da2ae1dc832bebd1a126e434f25d
CanadaHonk <oj@oojmed.com>
2023-06-25 10:05:10 +0100

    compiler: fix info flag check

--- compiler/index.js
+++ compiler/index.js
@@ -6,21 +6,21 @@ import produceSections from './sections.js';
 export default (code, flags = [ 'module' ]) => {
   const t0 = performance.now();
   const program = parse(code, flags);
-  if (flags.info) console.log(`1. parsed in ${(performance.now() - t0).toFixed(2)}ms`);
+  if (flags.includes('info')) console.log(`1. parsed in ${(performance.now() - t0).toFixed(2)}ms`);
 
   const t1 = performance.now();
   const { funcs, globals } = codeGen(program);
-  if (flags.info) console.log(`2. generated code in ${(performance.now() - t1).toFixed(2)}ms`);
+  if (flags.includes('info')) console.log(`2. generated code in ${(performance.now() - t1).toFixed(2)}ms`);
 
   const t2 = performance.now();
   for (const f of funcs) {
     f.wasm = opt(f.wasm);
   }
-  if (flags.info) console.log(`3. optimized code in ${(performance.now() - t2).toFixed(2)}ms`);
+  if (flags.includes('info')) console.log(`3. optimized code in ${(performance.now() - t2).toFixed(2)}ms`);
 
   const t3 = performance.now();
   const sections = produceSections(funcs, globals, flags);
-  if (flags.info) console.log(`4. produced sections in ${(performance.now() - t3).toFixed(2)}ms`);
+  if (flags.includes('info')) console.log(`4. produced sections in ${(performance.now() - t3).toFixed(2)}ms`);
 
   return sections;
 };
