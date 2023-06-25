d9492ae05c6ae53bc2cd5a0a1fc9e8bd982c44bf
CanadaHonk <oj@oojmed.com>
2023-06-25 18:48:37 +0100

    test: rewrite handler to run each with multiple args

--- test/index.js
+++ test/index.js
@@ -9,6 +9,8 @@ const run = async source => {
   const wasm = compile(source);
   times.push(performance.now() - t0);
 
+  // fs.writeFileSync('out.wasm', Buffer.from(wasm));
+
   totalOutput += wasm.byteLength;
 
   let out = '';
@@ -16,7 +18,8 @@ const run = async source => {
 
   const { instance } = await WebAssembly.instantiate(wasm, {
     '': {
-      p: i => print(i.toString())
+      p: i => print(i.toString()),
+      c: i => print(String.fromCharCode(i))
     }
   });
 
@@ -27,29 +30,40 @@ const run = async source => {
   return [ out, times, wasm ];
 };
 
-const t0 = performance.now();
-
-let total = 0, passes = 0;
-for (const test of fs.readdirSync('test')) {
-  if (test === 'index.js') continue;
+const perform = async (test, args) => {
+  process.argv = process.argv.slice(0, 2).concat(args);
   const content = fs.readFileSync('test/' + test, 'utf8');
   const spl = content.split('\n');
   const expect = spl[0].slice(2).trim();
   const code = spl.slice(1).join('\n');
 
   const t1 = performance.now();
-  const [ out, times, wasm ] = await run(code);
+  let [ out, times, wasm ] = await run(code);
+  out = out.trim();
   const time = performance.now() - t1;
   const pass = out === expect;
 
   total++;
   if (pass) passes++;
 
-  console.log(`${pass ? '\u001b[92mPASS' : '\u001b[91mFAIL'} ${test}\u001b[0m ${' '.repeat(30 - test.length)}\u001b[90m${time.toFixed(2)}ms (compile: ${times[0].toFixed(2)}ms, exec: ${times[1].toFixed(2)}ms)${' '.repeat(10)}${wasm.byteLength}b\u001b[0m`);
+  console.log(`${pass ? '\u001b[92mPASS' : '\u001b[91mFAIL'} ${test}\u001b[0m ${args.join(' ')} ${' '.repeat(40 - test.length - args.join(' ').length)}\u001b[90m${time.toFixed(2)}ms (compile: ${times[0].toFixed(2)}ms, exec: ${times[1].toFixed(2)}ms)${' '.repeat(10)}${wasm.byteLength}b\u001b[0m`);
 
   if (!pass) {
-    console.log(`expected: ${expect}\n  got: ${out}\n`);
+    console.log(`expected: ${expect}\n     got: ${out}\n`);
   }
+
+  return pass;
+};
+
+const t0 = performance.now();
+
+let total = 0, passes = 0;
+for (const test of fs.readdirSync('test')) {
+  if (test === 'index.js') continue;
+  await perform(test, []);
+  await perform(test, [ '-O0' ]);
+  await perform(test, [ '-O1' ]);
+  await perform(test, [ '-O2' ]);
 }
 
 console.log(`\u001b[1m\n${passes}/${total} tests passed (took ${(performance.now() - t0).toFixed(2)}ms)\u001b[0m`);
