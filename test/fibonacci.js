78ce3a3912577cd4891a6eafdecd3ee9f9e9b569
CanadaHonk <oj@oojmed.com>
2023-06-25 18:48:23 +0100

    test: add fibonacci.js

---
+++ test/fibonacci.js
@@ -0,0 +1,16 @@
+// 0 1 1 2 3 5 8 13 21 34 55
+const p = x => {
+  print(x);
+  printChar(' ');
+};
+
+let a = 0, b = 1;
+p(a); p(b);
+
+for (let i = 2; i <= 10; i++) {
+  let t = b + a;
+  a = b;
+  b = t;
+
+  p(t);
+}
