8d367940e82cc14d369b2e2aed939fd6a110878f
CanadaHonk <oj@oojmed.com>
2023-06-26 23:31:10 +0100

    compare: initial add

---
+++ runner/compare.js
@@ -0,0 +1,35 @@
+import { execSync } from 'node:child_process';
+import { pathToFileURL } from 'node:url';
+import fs from 'node:fs';
+
+const [ commitsAgo, file ] = process.argv.slice(2);
+const source = fs.readFileSync(file, 'utf8');
+
+const compileNow = (await import('../compiler/index.js')).default;
+const wasmNow = compileNow(source).byteLength;
+
+execSync(`git checkout HEAD~${commitsAgo}`);
+
+let failed = false;
+
+fs.writeFileSync('tmp.js', source);
+try {
+  execSync(`node runner/index.js tmp.js`);
+} catch {
+  failed = true;
+} finally {
+  fs.rmSync('tmp.js');
+  execSync(`git checkout main`);
+}
+
+console.log();
+
+if (failed) {
+  console.log(`!! failed to compile then`);
+  process.exit();
+}
+
+const wasmThen = Buffer.byteLength(fs.readFileSync('out.wasm'));
+
+console.log(`now: ${wasmNow} bytes`);
+console.log(`${commitsAgo} commit${commitsAgo > 1 ? 's' : ''} ago: ${wasmThen} bytes`);
