585652b271d2bfdff314fe558d9ca34600552bef
CanadaHonk <oj@oojmed.com>
2023-06-29 01:00:54 +0100

    sections: fix no local detection

--- compiler/sections.js
+++ compiler/sections.js
@@ -85,7 +85,7 @@ export default (funcs, globals, flags) => {
   const codeSection = createSection(
     Section.code,
     encodeVector(funcs.map(x => {
-      const maxLocal = x.locals.length === 0 ? -1 : Math.max(...Object.values(x.locals));
+      const maxLocal = Object.keys(x.locals).length === 0 ? -1 : Math.max(...Object.values(x.locals));
       const localCount = (maxLocal + 1) - x.params.length;
       const localDecl = localCount > 0 ? [encodeLocal(localCount, Valtype[valtype])] : [];
       return encodeVector([ ...encodeVector(localDecl), ...x.wasm.flat().filter(x => x !== null), Opcodes.end ]);
