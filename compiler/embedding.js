9698a7ac304a3da5c9993583773ef0e3a11f53ad
CanadaHonk <oj@oojmed.com>
2023-06-29 05:41:20 +0100

    embedding: add i32_x4

--- compiler/embedding.js
+++ compiler/embedding.js
@@ -2,3 +2,12 @@ import { Opcodes } from "./wasmSpec.js";
 import { signedLEB128 } from "./encoding.js";
 
 export const number = n => [ [ Opcodes.const, ...signedLEB128(n) ] ];
+
+const enforceTwoBytes = arr => [ arr[0] ?? 0, arr[1] ?? 0, arr[2] ?? 0, arr[3] ?? 0 ];
+export const i32_x4 = (a, b, c, d) => [ [
+  ...Opcodes.v128_const,
+  ...enforceTwoBytes(signedLEB128(a)),
+  ...enforceTwoBytes(signedLEB128(b)),
+  ...enforceTwoBytes(signedLEB128(c)),
+  ...enforceTwoBytes(signedLEB128(d))
+] ];
