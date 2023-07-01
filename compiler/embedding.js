ca03c35e82e0c342dbb33449753731eef1926186
CanadaHonk <oj@oojmed.com>
2023-07-01 23:58:29 +0100

    embedding: tweak vec func naming

--- compiler/embedding.js
+++ compiler/embedding.js
@@ -4,7 +4,7 @@ import { signedLEB128 } from "./encoding.js";
 export const number = n => [ [ Opcodes.const, ...signedLEB128(n) ] ];
 
 const enforceTwoBytes = arr => [ arr[0] ?? 0, arr[1] ?? 0, arr[2] ?? 0, arr[3] ?? 0 ];
-export const i32_x4 = (a, b, c, d) => [ [
+export const i32x4 = (a, b, c, d) => [ [
   ...Opcodes.v128_const,
   ...enforceTwoBytes(signedLEB128(a)),
   ...enforceTwoBytes(signedLEB128(b)),
