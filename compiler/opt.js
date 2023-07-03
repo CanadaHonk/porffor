a4ea1238fd74f516e04ab570efdd874c29610e27
CanadaHonk <oj@oojmed.com>
2023-07-03 01:19:00 +0100

    opt: fix inline local indexing

--- compiler/opt.js
+++ compiler/opt.js
@@ -57,7 +57,7 @@ export default (funcs, globals) => {
 
             // add params as locals and set in reverse order
             const paramIdx = {};
-            let localIdx = Math.max(0, ...Object.values(t.locals).map(x => x.idx));
+            let localIdx = Math.max(0, ...Object.values(t.locals).map(x => x.idx)) + 1;
             for (let j = c.params.length - 1; j >= 0; j--) {
               const name = `__porf_inline_${c.name}_param_${j}`;
 
