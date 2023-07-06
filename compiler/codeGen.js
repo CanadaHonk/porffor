89aae7db5079fb7f11dedad54c47b6da52625406
CanadaHonk <oj@oojmed.com>
2023-07-06 13:44:07 +0100

    codegen: fix error

--- compiler/codeGen.js
+++ compiler/codeGen.js
@@ -405,8 +405,8 @@ const generateCall = (scope, decl) => {
     args = args.slice(0, func.params.length);
   }
 
-  if (func.memory) scope.memory = true;
-  if (func.throws) scope.throws = true;
+  if (func && func.memory) scope.memory = true;
+  if (func && func.throws) scope.throws = true;
 
   const out = [];
   for (const arg of args) {
