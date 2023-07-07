75959285acbbf89255d08c054d1564904e9c03b2
CanadaHonk <oj@oojmed.com>
2023-07-07 01:16:54 +0100

    test: add isNaN tests

---
+++ test/float_isnan.js
@@ -0,0 +1,11 @@
+// "000011"
+print(isNaN(0));
+print(isNaN(1));
+
+print(isNaN(Infinity));
+print(isNaN(-Infinity));
+
+print(isNaN(NaN));
+
+let a = NaN;
+print(isNaN(a));
---
+++ test/float_number_isnan.js
@@ -0,0 +1,11 @@
+// "000011"
+print(Number.isNaN(0));
+print(Number.isNaN(1));
+
+print(Number.isNaN(Infinity));
+print(Number.isNaN(-Infinity));
+
+print(Number.isNaN(NaN));
+
+let a = NaN;
+print(Number.isNaN(a));
