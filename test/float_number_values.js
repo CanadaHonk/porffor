724e736ad71df7a286e0d5cd1d6bede0b78f8fd6
CanadaHonk <oj@oojmed.com>
2023-07-08 00:20:08 +0100

    test: add number min/max value tests

---
+++ test/float_number_values.js
@@ -0,0 +1,3 @@
+// "1.7976931348623157e+308\n5e-324\n"
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
---
+++ test/int_number_values.js
@@ -0,0 +1,3 @@
+// "2147483647\n-2147483648\n"
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
