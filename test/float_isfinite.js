d6674c467964910c6b4bac52054ae9ec4af88e10
CanadaHonk <oj@oojmed.com>
2023-07-07 01:38:09 +0100

    test: add isFinite test

---
+++ test/float_isfinite.js
@@ -0,0 +1,29 @@
+// "1100000011000000"
+
+let a = Infinity;
+let b = -Infinity;
+let c = NaN;
+
+print(isFinite(0));
+print(isFinite(1));
+
+print(isFinite(Infinity));
+print(isFinite(a));
+
+print(isFinite(-Infinity));
+print(isFinite(b));
+
+print(isFinite(NaN));
+print(isFinite(c));
+
+print(Number.isFinite(0));
+print(Number.isFinite(1));
+
+print(Number.isFinite(Infinity));
+print(Number.isFinite(a));
+
+print(Number.isFinite(-Infinity));
+print(Number.isFinite(b));
+
+print(Number.isFinite(NaN));
+print(Number.isFinite(c));
