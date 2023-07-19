c8bc1a25d7875bb679c62e09b724f82a28fad784
CanadaHonk <oj@oojmed.com>
2023-07-19 22:11:57 +0100

    test: add Math.imul tests
    test262: 10.72% | 🧪 48843 | 🤠 5234 | ❌ 919 | 💀 7829 | 🧩 74 | 💥 3090 | 📝 31697

--- test/float_math_funcs.js
+++ test/float_math_funcs.js
@@ -1,4 +1,4 @@
-// "2\n2\n4\n5\n4\n5\n1\n-1\n31\n29\n3.5\n5.050000190734863\n"
+// "2\n2\n4\n5\n4\n5\n1\n-1\n31\n29\n3.5\n5.050000190734863\n12\n-60\n"
 
 console.log(Math.sqrt(4));
 
@@ -18,3 +18,7 @@ console.log(Math.clz32(4));
 
 console.log(Math.fround(3.5));
 console.log(Math.fround(5.05));
+
+console.log(Math.imul(3, 4));
+console.log(Math.imul(-5, 12));
+// console.log(Math.imul(0xffffffff, 5));
