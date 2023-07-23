2b4f57c5ca7d1873d0ee034d512b43312cba21d5
github-action-benchmark <github@users.noreply.github.com>
2023-07-23 22:06:03 +0000

    add porffor benches (benchmarkjs) benchmark result for 00c47ec94f85e7bc42a894d4ebf711db61785c70

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1690149451525,
+  "lastUpdate": 1690149963847,
   "repoUrl": "https://github.com/CanadaHonk/porffor",
   "entries": {
     "porffor benches": [
@@ -115,6 +115,121 @@ window.BENCHMARK_DATA = {
             "range": "±0.16%",
             "unit": "ops/sec",
             "extra": "99 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "oj@oojmed.com",
+            "name": "CanadaHonk",
+            "username": "CanadaHonk"
+          },
+          "committer": {
+            "email": "oj@oojmed.com",
+            "name": "CanadaHonk",
+            "username": "CanadaHonk"
+          },
+          "distinct": true,
+          "id": "00c47ec94f85e7bc42a894d4ebf711db61785c70",
+          "message": "bench ci: set title",
+          "timestamp": "2023-07-23T23:04:12+01:00",
+          "tree_id": "7789a14fd9fb5d86475135aab062748f35e0339c",
+          "url": "https://github.com/CanadaHonk/porffor/commit/00c47ec94f85e7bc42a894d4ebf711db61785c70"
+        },
+        "date": 1690149963827,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "node countPrimes(10000)",
+            "value": 39.63,
+            "range": "±0.02%",
+            "unit": "ops/sec",
+            "extra": "68 samples"
+          },
+          {
+            "name": "porffor countPrimes(10000)",
+            "value": 48.19,
+            "range": "±0.09%",
+            "unit": "ops/sec",
+            "extra": "63 samples"
+          },
+          {
+            "name": "porffor(i32) countPrimes(10000)",
+            "value": 44.69,
+            "range": "±0.06%",
+            "unit": "ops/sec",
+            "extra": "59 samples"
+          },
+          {
+            "name": "node randoms(100000)",
+            "value": 873,
+            "range": "±0.12%",
+            "unit": "ops/sec",
+            "extra": "95 samples"
+          },
+          {
+            "name": "porffor randoms(100000)",
+            "value": 1985,
+            "range": "±0.02%",
+            "unit": "ops/sec",
+            "extra": "98 samples"
+          },
+          {
+            "name": "node factorial(100)",
+            "value": 635098,
+            "range": "±0.08%",
+            "unit": "ops/sec",
+            "extra": "97 samples"
+          },
+          {
+            "name": "porffor factorial(100)",
+            "value": 700216,
+            "range": "±0.11%",
+            "unit": "ops/sec",
+            "extra": "97 samples"
+          },
+          {
+            "name": "node arrayAccess(45)",
+            "value": 706379271,
+            "range": "±0.22%",
+            "unit": "ops/sec",
+            "extra": "94 samples"
+          },
+          {
+            "name": "porffor arrayAccess(45)",
+            "value": 1886,
+            "range": "±117.76%",
+            "unit": "ops/sec",
+            "extra": "7 samples"
+          },
+          {
+            "name": "porffor(i32) arrayAccess(45)",
+            "value": 963,
+            "range": "±178.97%",
+            "unit": "ops/sec",
+            "extra": "7 samples"
+          },
+          {
+            "name": "node arithmetic(45)",
+            "value": 707641059,
+            "range": "±0.13%",
+            "unit": "ops/sec",
+            "extra": "98 samples"
+          },
+          {
+            "name": "porffor arithmetic(45)",
+            "value": 1973769,
+            "range": "±0.35%",
+            "unit": "ops/sec",
+            "extra": "94 samples"
+          },
+          {
+            "name": "porffor(i32) arithmetic(45)",
+            "value": 1968041,
+            "range": "±0.25%",
+            "unit": "ops/sec",
+            "extra": "92 samples"
           }
         ]
       }
