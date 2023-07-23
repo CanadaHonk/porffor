92abc7ea2bf46a4982f3f7312bb20226ac21c3a9
github-action-benchmark <github@users.noreply.github.com>
2023-07-23 20:00:49 +0000

    add porffor benches (benchmarkjs) benchmark result for 37972f82f15efd12c9bd7a7baadf023a9965932c

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1690141839438,
+  "lastUpdate": 1690142449902,
   "repoUrl": "https://github.com/CanadaHonk/porffor",
   "entries": {
     "porffor benches": [
@@ -59,6 +59,65 @@ window.BENCHMARK_DATA = {
             "range": "±0.17%",
             "unit": "ops/sec",
             "extra": "98 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "oj@oojmed.com",
+            "name": "CanadaHonk",
+            "username": "CanadaHonk"
+          },
+          "committer": {
+            "email": "oj@oojmed.com",
+            "name": "CanadaHonk",
+            "username": "CanadaHonk"
+          },
+          "distinct": true,
+          "id": "37972f82f15efd12c9bd7a7baadf023a9965932c",
+          "message": "bench ci: display tweaks",
+          "timestamp": "2023-07-23T20:59:54+01:00",
+          "tree_id": "12ae101747f725c1eae9d1dc6298fbe49e3200ad",
+          "url": "https://github.com/CanadaHonk/porffor/commit/37972f82f15efd12c9bd7a7baadf023a9965932c"
+        },
+        "date": 1690142449881,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "node countPrimes(10000)",
+            "value": 33.06,
+            "range": "±0.13%",
+            "unit": "ops/sec",
+            "extra": "58 samples"
+          },
+          {
+            "name": "porffor(default) countPrimes(10000)",
+            "value": 40.05,
+            "range": "±1.14%",
+            "unit": "ops/sec",
+            "extra": "53 samples"
+          },
+          {
+            "name": "porffor(i32) countPrimes(10000)",
+            "value": 37.44,
+            "range": "±0.15%",
+            "unit": "ops/sec",
+            "extra": "65 samples"
+          },
+          {
+            "name": "node randoms(10000)",
+            "value": 7305,
+            "range": "±0.17%",
+            "unit": "ops/sec",
+            "extra": "94 samples"
+          },
+          {
+            "name": "porffor(default) randoms(10000)",
+            "value": 16305,
+            "range": "±0.25%",
+            "unit": "ops/sec",
+            "extra": "90 samples"
           }
         ]
       }
