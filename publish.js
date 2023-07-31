06503501c8ffaff03e0b7d0033693e85f0e68769
CanadaHonk <oj@oojmed.com>
2023-08-01 00:10:56 +0100

    chore: tweak publishing
    test262: 10.96% | 🧪 48843 | 🤠 5353 | ❌ 1126 | 💀 8961 | 🧩 279 | 💥 3498 | ⏰ 1 | 📝 29625

--- publish.js
+++ publish.js
@@ -6,6 +6,8 @@ const rev = fs.readFileSync('.git/refs/heads/main', 'utf8').trim().slice(0, 7);
 const packageJson = fs.readFileSync('package.json', 'utf8');
 fs.writeFileSync('package.json', packageJson.replace('"0.0.0"', `"0.0.0-${rev}"`));
 
+console.log(rev, packageJson);
+
 execSync(`npm publish`, { stdio: 'inherit' });
 
-execSync(`git checkout HEAD -- package.json`, { stdio: 'inherit' });
+fs.writeFileSync('package.json', packageJson);
