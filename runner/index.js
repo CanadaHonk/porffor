ebc04915203a951da1289dc9f9add20279ecfd06
CanadaHonk <oj@oojmed.com>
2023-08-10 17:35:06 +0100

    runner: add -v and v8 flags if compile hints on
    test262: 11.05% | 🧪 48843 | 🤠 5398 | ❌ 1090 | 💀 9006 | 🧩 246 | 💥 3428 | ⏰ 1 | 📝 29674

--- runner/index.js
+++ runner/index.js
@@ -3,8 +3,26 @@
 import compile from '../compiler/wrap.js';
 import fs from 'node:fs';
 
+if (process.argv.includes('-compile-hints')) {
+  const v8 = await import('node:v8');
+  v8.setFlagsFromString(`--experimental-wasm-compilation-hints`);
+
+  // see also these flags:
+  // --experimental-wasm-branch-hinting
+  // --experimental-wasm-extended-const
+  // --experimental-wasm-inlining (?)
+  // --experimental-wasm-js-inlining (?)
+  // --experimental-wasm-return-call (on by default)
+}
+
 const file = process.argv.slice(2).find(x => x[0] !== '-');
 if (!file) {
+  if (process.argv.includes('-v')) {
+    // just print version
+    console.log((await import('./version.js')).default);
+    process.exit(0);
+  }
+
   // run repl if no file given
   await import('./repl.js');
 
@@ -30,5 +48,6 @@ try {
   exports.main();
   if (cache) process.stdout.write(cache);
 } catch (e) {
+  if (cache) process.stdout.write(cache);
   console.error(`${e.constructor.name}: ${e.message}`);
 }
--- runner/repl.js
+++ runner/repl.js
@@ -1,14 +1,7 @@
 import compile from '../compiler/wrap.js';
+import rev from './version.js';
 
 import repl from 'node:repl';
-import fs from 'node:fs';
-
-let rev = 'unknown';
-try {
-  rev = fs.readFileSync(new URL('../.git/refs/heads/main', import.meta.url), 'utf8').trim().slice(0, 7);
-} catch {
-  rev = JSON.parse(fs.readFileSync(new URL('../package.json', import.meta.url), 'utf8')).version.split('-')[1];
-}
 
 // process.argv.push('-O0'); // disable opts
 
