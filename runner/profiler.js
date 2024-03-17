6a7468dd4610ab5a5a4b4ca65d5fe20375d28ba7
CanadaHonk <honk@goose.icu>
2024-03-17 22:44:29 +0000

    profiler: add fast and slow modes

--- runner/profiler.js
+++ runner/profiler.js
@@ -3,25 +3,47 @@
 import compile from '../compiler/wrap.js';
 import fs from 'node:fs';
 
-globalThis.profiler = true;
+import Prefs from '../compiler/prefs.js';
+
+const fast = Prefs.profiler === 'fast';
 
 const file = process.argv.slice(2).find(x => x[0] !== '-');
 let source = fs.readFileSync(file, 'utf8');
 
 let profileId = 0;
-source = source.replace(/^[^\n}]*;$/mg, _ => `profile(${profileId++});${_}profile(${profileId++});`);
+source = fast ? source.replace(/^[^\n}]*;$/mg, _ => `profile(${profileId++});${_}profile(${profileId++});`) : source.replace(/^[^\n}]*;$/mg, _ => `profile(${profileId++});profile(${profileId++});${_}profile(${profileId++});`);
 
 // console.log(source);
 
-let tmp = {};
+let tmp = new Array(profileId).fill(0);
+let samples = 0;
 
 const percents = process.argv.includes('-%');
 
+const spinner = ['-', '\\', '|', '/'];
+let spin = 0;
+let last = 0;
+
 try {
   const { exports } = await compile(source, process.argv.includes('--module') ? [ 'module' ] : [], {
-    z: n => {
-      if (n % 2) tmp[n] = (tmp[n] ?? 0) + (performance.now() - tmp[n - 1]);
-        else tmp[n] = performance.now();
+    z: fast ? n => {
+      if (n % 2) {
+        tmp[n] += performance.now() - tmp[n - 1];
+      } else {
+        tmp[n] = performance.now();
+      }
+    } : n => {
+      if (n % 3 === 2) {
+        tmp[n] += (performance.now() - tmp[n - 1]) - (tmp[n - 1] - tmp[n - 2]);
+        samples++;
+
+        if (performance.now() > last) {
+          process.stdout.write(`\r${spinner[spin++ % 4]} running: collected ${samples} samples...`);
+          last = performance.now() + 100;
+        }
+      } else {
+        tmp[n] = performance.now();
+      }
     }
   });
 
@@ -31,11 +53,11 @@ try {
 
   const total = performance.now() - start;
 
-  console.log('\n\ntotal: ' + total + '\n\n' + source.split('\n').map(x => {
+  console.log(`\nsamples: ${fast ? 'not measured' : samples}\ntotal: ${total}ms\n\n` + source.split('\n').map(x => {
     let time = 0;
     if (x.startsWith('profile')) {
       const id = parseInt(x.slice(8, x.indexOf(')')));
-      time = tmp[id + 1];
+      time = fast ? tmp[id + 1] : tmp[id + 2];
     }
 
     let color = [ 0, 0, 0 ];
