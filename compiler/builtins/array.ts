d242e6357b25ca3abd54152c42329b5b987abcc6
CanadaHonk <honk@goose.icu>
2024-04-14 14:17:26 +0100

    builtins/array: cleanup

--- compiler/builtins/array.ts
+++ compiler/builtins/array.ts
@@ -1,3 +1,4 @@
 // @porf -funsafe-no-unlikely-proto-checks -valtype=i32
 
-export const __Array_isArray = (x: unknown): boolean => Porffor.wasm`local.get ${x+1}` == Porffor.TYPES._array;
+export const __Array_isArray = (x: unknown): boolean =>
+  Porffor.wasm`local.get ${x+1}` == Porffor.TYPES._array;
