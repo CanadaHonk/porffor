eb347d2503681fdf0be795744aa4b8ebc15acbc5
CanadaHonk <honk@goose.icu>
2024-05-02 00:36:58 +0100

    builtins/int: impl prefix +/- support
    test262: 13.52% | 🧪 50005 | 🤠 6762 (+1) | ❌ 1806 (-3) | 💀 13981 (+1) | 🏗️ 2503 (+1) | 💥 2550 | ⏰ 2 | 📝 22401

--- compiler/builtins/int.ts
+++ compiler/builtins/int.ts
@@ -6,11 +6,24 @@ export const parseInt = (input: string|bytestring): f64 => {
 
   let n: f64 = NaN;
 
-  let i: f64 = Porffor.wasm`local.get ${input}`;
-  const len: f64 = Porffor.wasm.i32.load(input, 0, 0);
+  const inputPtr: f64 = Porffor.wasm`local.get ${input}`;
+  const len: f64 = Porffor.wasm.i32.load(inputPtr, 0, 0);
+  let i: f64 = inputPtr;
+
+  let negative: boolean = false;
 
   if (Porffor.rawType(input) == Porffor.TYPES._bytestring) {
     const endPtr: f64 = i + len;
+
+    // check start of string
+    const startChr: f64 = Porffor.wasm.i32.load8_u(i, 0, 4);
+
+    if (startChr == 43) i++;
+    if (startChr == 45) {
+      negative = true;
+      i++;
+    }
+
     while (i < endPtr) {
       const chr: f64 = Porffor.wasm.i32.load8_u(i++, 0, 4);
 
@@ -19,13 +32,27 @@ export const parseInt = (input: string|bytestring): f64 => {
 
         n *= 10;
         n += chr - 48;
-      } else return n;
+      } else {
+        if (negative) return -n;
+        return n;
+      }
     }
 
+    if (negative) return -n;
     return n;
   }
 
   const endPtr: f64 = i + len * 2;
+
+  // check start of string
+  const startChr: f64 = Porffor.wasm.i32.load16_u(i, 0, 4);
+
+  if (startChr == 43) i += 2;
+  if (startChr == 45) {
+    negative = true;
+    i += 2;
+  }
+
   while (i < endPtr) {
     const chr: f64 = Porffor.wasm.i32.load16_u(i, 0, 4);
     i += 2;
@@ -35,8 +62,12 @@ export const parseInt = (input: string|bytestring): f64 => {
 
       n *= 10;
       n += chr - 48;
-    } else return n;
+    } else {
+      if (negative) return -n;
+      return n;
+    }
   }
 
+  if (negative) return -n;
   return n;
 };
