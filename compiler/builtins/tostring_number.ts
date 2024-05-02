45d4ae2323f2b656677c468594ce939a8726b368
CanadaHonk <honk@goose.icu>
2024-05-03 00:02:57 +0100

    builtins/tostring_number: impl radix
    test262: 13.64% | 🧪 50005 | 🤠 6820 (+1) | ❌ 1851 (-1) | 💀 14103 | 🏗️ 1641 | 💥 2550 | ⏰ 2 | 📝 23038

--- compiler/builtins/tostring_number.ts
+++ compiler/builtins/tostring_number.ts
@@ -1,9 +1,20 @@
 // // @porf -funsafe-no-unlikely-proto-checks
 
-export const __Number_prototype_toString = (_this: number, radix: number) => {
-  // todo: use radix
+// radix: number|any for rawType check
+export const __Number_prototype_toString = (_this: number, radix: number|any) => {
+  // todo: use exponential notation if >=1e21 (?)
+
+  if (Porffor.rawType(radix) != Porffor.TYPES.number) {
+    // todo: string to number
+    radix = 10;
+  }
 
   let out: bytestring = '';
+
+  if (radix < 2 || radix > 36) {
+    // todo: throw RangeError: toString() radix argument must be between 2 and 36
+    return out;
+  }
 
   if (!Number.isFinite(_this)) {
     if (Number.isNaN(_this)) out = 'NaN';
@@ -22,18 +33,21 @@ export const __Number_prototype_toString = (_this: number, radix: number) => {
 
     let l: i32 = 0;
     for (; i > 0; l++) {
-      let d: i32 = i % 10;
+      Porffor.wasm.i32.store8(Porffor.wasm`local.get ${digits}` + l, i % radix, 0, 4);
 
-      Porffor.wasm.i32.store8(Porffor.wasm`local.get ${digits}` + l, d, 0, 4);
-
-      i = (i / 10) | 0;
+      i = (i / radix) | 0;
     }
 
     let outPtr: i32 = Porffor.wasm`local.get ${out}`;
     let digitsPtr: i32 = Porffor.wasm`local.get ${digits}` + l;
     const endPtr: i32 = outPtr + l;
     while (outPtr < endPtr) {
-      Porffor.wasm.i32.store8(outPtr++, 48 + Porffor.wasm.i32.load8_u(--digitsPtr, 0, 4), 0, 4);
+      let digit: i32 = Porffor.wasm.i32.load8_u(--digitsPtr, 0, 4);
+
+      if (digit < 10) digit += 48; // 0-9
+        else digit += 87; // a-z
+
+      Porffor.wasm.i32.store8(outPtr++, digit, 0, 4);
     }
 
     out.length = l;
