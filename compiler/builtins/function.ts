2afd28c85ca84cc478b4376a94abf1bbd4918af9
CanadaHonk <honk@goose.icu>
2024-05-13 00:25:58 +0100

    builtins/tostring: split into per-type files
    test262: 14.23% | 🧪 50012 | 🤠 7118 | ❌ 2023 | 💀 14286 | 🏗️ 778 | 💥 1628 | ⏰ 2 | 📝 24177

---
+++ compiler/builtins/function.ts
@@ -0,0 +1,7 @@
+// // @porf --funsafe-no-unlikely-proto-checks
+
+export const __Function_prototype_toString = (_this: Function) => {
+  // todo: actually use source
+  let out: bytestring = 'function () {}';
+  return out;
+};
---
+++ compiler/builtins/object.ts
@@ -0,0 +1,6 @@
+// // @porf --funsafe-no-unlikely-proto-checks
+
+export const __Object_prototype_toString = (_this: object) => {
+  let out: bytestring = '[object Object]';
+  return out;
+};
