627f05742948d2713bf767d237bcb92e5a687b15
Linus Groh <mail@linusgroh.de>
2024-05-12 22:26:37 +0100

    builtins/boolean: implement valueOf
    test262: 14.21% | 🧪 50012 (+7) | 🤠 7109 (+2) | ❌ 1959 (+2) | 💀 14360 | 🏗️ 778 (+2) | 💥 1628 | ⏰ 2 | 📝 24176 (+1)

---
+++ compiler/builtins/boolean.ts
@@ -0,0 +1,6 @@
+// 20.3.3.3 Boolean.prototype.valueOf ()
+// https://tc39.es/ecma262/#sec-boolean.prototype.valueof
+export const __Boolean_prototype_valueOf = (_this: boolean) => {
+  // 1. Return ? ThisBooleanValue(this value).
+  return _this;
+};
