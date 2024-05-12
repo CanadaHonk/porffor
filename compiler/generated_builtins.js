8ccd1870f1f264a33d3a39df0e4256f2eda289e3
Linus Groh <mail@linusgroh.de>
2024-05-12 22:35:02 +0100

    builtins/string: implement valueOf
    test262: 14.22% | 🧪 50012 | 🤠 7112 | ❌ 1959 | 💀 14356 | 🏗️ 778 | 💥 1628 | ⏰ 2 | 📝 24177

--- compiler/generated_builtins.js
+++ compiler/generated_builtins.js
@@ -1449,6 +1449,24 @@ export const BuiltinFuncs = function() {
     locals: [127],
     localNames: ["_this","_this#type","#last_type"],
   };
+  this.__String_prototype_valueOf = {
+    wasm: (scope, { allocPage, builtin }) => [[32,0],[65,2],[15]],
+    params: [127,127],
+    typedParams: true,
+    returns: [127,127],
+    typedReturns: true,
+    locals: [],
+    localNames: ["_this","_this#type"],
+  };
+  this.__ByteString_prototype_valueOf = {
+    wasm: (scope, { allocPage, builtin }) => [[32,0],[65,18],[15]],
+    params: [127,127],
+    typedParams: true,
+    returns: [127,127],
+    typedReturns: true,
+    locals: [],
+    localNames: ["_this","_this#type"],
+  };
   this.__Boolean_prototype_toString = {
     wasm: (scope, { allocPage, builtin }) => [...number(allocPage(scope, 'bytestring: __Boolean_prototype_toString/out', 'i8') * pageSize, 124),[33,2],[32,0],[252,3],[4,64],[32,2],[252,3],[34,3],[65,4],[54,1,0],[32,3],[65,244,0],[58,0,4],[32,3],[65,242,0],[58,0,5],[32,3],[65,245,0],[58,0,6],[32,3],[65,229,0],[58,0,7],[32,3],[184],[33,2],[5],[32,2],[252,3],[34,3],[65,5],[54,1,0],[32,3],[65,230,0],[58,0,4],[32,3],[65,225,0],[58,0,5],[32,3],[65,236,0],[58,0,6],[32,3],[65,243,0],[58,0,7],[32,3],[65,229,0],[58,0,8],[32,3],[184],[33,2],[11],[32,2],[65,18],[15]],
     params: [124,127],
