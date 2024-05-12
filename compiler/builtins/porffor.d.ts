3aaa9b4ee7a0e6d7d740e4d6f3c465e60ab40dd2
CanadaHonk <honk@goose.icu>
2024-05-12 11:49:07 +0100

    builtins/typedef: update

--- compiler/builtins/porffor.d.ts
+++ compiler/builtins/porffor.d.ts
@@ -7,23 +7,40 @@ type PorfforGlobal = {
   wasm: {
     (...args: any[]): any;
     i32: {
-      or(a: i32, b: i32): i32;
-
       load(pointer: i32, align: i32, offset: i32): i32;
       store(pointer: i32, value: i32, align: i32, offset: i32): i32;
       load8_u(pointer: i32, align: i32, offset: i32): i32;
       store8(pointer: i32, value: i32, align: i32, offset: i32): i32;
       load16_u(pointer: i32, align: i32, offset: i32): i32;
       store16(pointer: i32, value: i32, align: i32, offset: i32): i32;
+      const(value: i32): i32;
+    }
+
+    f64: {
+      load(pointer: i32, align: i32, offset: i32): i32;
+      store(pointer: i32, value: f64, align: i32, offset: i32): f64;
     }
   }
 
-  // randomInt(): i32;
   randomByte(): i32;
 
   type(x: any): bytestring;
   rawType(x: any): i32;
-  TYPES: Record<string, i32>;
+  TYPES: {
+    number: i32;
+    boolean: i32;
+    string: i32;
+    undefined: i32;
+    object: i32;
+    function: i32;
+    symbol: i32;
+    bigint: i32;
+
+    _array: i32;
+    _regexp: i32;
+    _bytestring: i32;
+    _date: i32;
+  }
 
   fastOr(...args: any): boolean;
   fastAnd(...args: any): boolean;
