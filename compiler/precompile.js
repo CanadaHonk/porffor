e1a34c10519aa747d24aab1552c1df26adb3b1e7
CanadaHonk <honk@goose.icu>
2024-05-16 12:07:40 +0100

    prefs: disable aotPointerOpt by default
    test262: 14.63% (+0.01) | 🧪 50012 | 🤠 7318 (+4) | ❌ 2204 | 💀 15711 (-4) | 🏗️ 643 | 💥 1628 | ⏰ 1 | 📝 22507

--- compiler/precompile.js
+++ compiler/precompile.js
@@ -18,7 +18,7 @@ const compile = async (file, [ _funcs, _globals ]) => {
     first = source.slice(0, source.indexOf('\n'));
   }
 
-  let args = ['--bytestring', '--todo-time=compile', '--no-aot-pointer-opt', '--no-indirect-calls', '--no-treeshake-wasm-imports', '--no-rm-unused-types', '--scoped-page-names', '--funsafe-no-unlikely-proto-checks', '--parse-types', '--opt-types'];
+  let args = ['--bytestring', '--todo-time=compile', '--no-indirect-calls', '--no-treeshake-wasm-imports', '--no-rm-unused-types', '--scoped-page-names', '--funsafe-no-unlikely-proto-checks', '--parse-types', '--opt-types'];
   if (first.startsWith('// @porf')) {
     args = args.concat(first.slice('// @porf '.length).split(' '));
   }
