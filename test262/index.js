8107e135a7b147f6a5cbb879f2f5b1a75fa2430c
CanadaHonk <honk@goose.icu>
2024-05-23 16:43:43 +0100

    test262: add more to timeout list

--- test262/index.js
+++ test262/index.js
@@ -411,7 +411,11 @@ if (isMainThread) {
 
     try {
       // only timeout some due to big perf impact
-      if (file.includes('while/') || file.includes('for/') || file.includes('continue/') || file.includes('break/') || file.includes('pow/') || file.endsWith('NumberFormat/constructor-unit.js')) timeout(exports.m, 500);
+      if (
+        file.includes('while/') || file.includes('for/') || file.includes('continue/') || file.includes('break/') ||
+        file.includes('pow/') || file.includes('generator/') ||
+        file.endsWith('NumberFormat/constructor-unit.js')
+      ) timeout(exports.m, 500);
         else exports.m();
       // timeout(exports.m, 500);
     } catch (e) {
