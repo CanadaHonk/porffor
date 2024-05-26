bf4206d7b6286d15ead0c6f755b014c9253f3773
CanadaHonk <honk@goose.icu>
2024-05-26 11:56:01 +0100

    builtins/base64: cleanup

--- compiler/builtins/base64.ts
+++ compiler/builtins/base64.ts
@@ -2,6 +2,8 @@
 import type {} from './porffor.d.ts';
 
 export const btoa = (input: bytestring): bytestring => {
+  // todo: throw on invalid chars
+
   const keyStr: bytestring = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
   const keyStrPtr: i32 = Porffor.wasm`local.get ${keyStr}`;
 
@@ -41,9 +43,9 @@ export const btoa = (input: bytestring): bytestring => {
   return output;
 };
 
-// todo: impl atob by converting below to "porf ts"
 export const atob = (input: bytestring): bytestring => {
-  // todo: handle non-base64 chars properly
+  // todo: throw on non-base64 chars
+
   const lut: bytestring = '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@>@@@?456789:;<=@@@@@@@\x00\x01\x02\x03\x04\x05\x06\x07\b\t\n\x0B\f\r\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19@@@@@@\x1A\x1B\x1C\x1D\x1E\x1F !"#$%&\'()*+,-./0123';
   const lutPtr: i32 = Porffor.wasm`local.get ${lut}`;
 
