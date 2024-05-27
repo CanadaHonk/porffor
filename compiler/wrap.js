ce76cbf3fa99ed0386ac4eb8d8b688a97347e761
CanadaHonk <honk@goose.icu>
2024-05-27 08:01:42 +0100

    wrap: disable auto-module

--- compiler/wrap.js
+++ compiler/wrap.js
@@ -140,7 +140,7 @@ export default (source, flags = [ 'module' ], customImports = {}, print = str =>
 
   globalThis.porfDebugInfo = { funcs, globals };
 
-  if (process.argv[1].includes('/runner') && source.includes?.('export ')) flags.push('module');
+  // if (process.argv[1].includes('/runner') && source.includes?.('export ')) flags.push('module');
 
   // fs.writeFileSync('out.wasm', Buffer.from(wasm));
 
