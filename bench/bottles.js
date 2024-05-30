d5f1bf7de30e9699968c692ddfddaaf35f9f0276
CanadaHonk <honk@goose.icu>
2024-05-30 21:48:58 +0100

    bench/bottles: fix

--- bench/bottles.js
+++ bench/bottles.js
@@ -1,5 +1,5 @@
 let beer = 99;
 while (beer > 0) {
-  printStr(beer + ' bottles of beer on the wall,\n' + beer + ' bottles of beer!\nTake one down, pass it around\n' + (beer - 1) + ' bottles of beer on the wall\n');
+  console.log(beer + ' bottles of beer on the wall,\n' + beer + ' bottles of beer!\nTake one down, pass it around\n' + (beer - 1) + ' bottles of beer on the wall\n');
   beer--;
 }
