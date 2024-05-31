4fd2db11c08a3b32d31d56df696a5eec963540fd
CanadaHonk <honk@goose.icu>
2024-05-31 11:48:37 +0100

    version: bump minor to 18

--- runner/index.js
+++ runner/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import fs from 'node:fs';
-globalThis.version = '0.17.3+3d806f394';
+globalThis.version = '0.18.1+984caf2bf';
 
 // deno compat
 if (typeof process === 'undefined' && typeof Deno !== 'undefined') {
