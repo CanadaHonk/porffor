3f04959b57da63e49d15e34baeb617f18c5033ca
Bob Varioa <true.email001@gmail.com>
2024-06-07 09:08:06 -0500

    builtins/symbol: fix symbol implementation

--- compiler/builtins/symbol.ts
+++ compiler/builtins/symbol.ts
@@ -1,34 +1,39 @@
 import type {} from './porffor.d.ts';
 
-export const __Porffor_symbol_descStore = (op: boolean, value: any): any => {
-  const ptr = Porffor.allocatePage<Symbol>();
+// todo: growing behavior, currently we are limited to 16384 symbols 
+export const __Porffor_symbol_create = (value: any): any => {
+  const ptr: number = Porffor.allocateNamedPage<any>("symbol");
+  const size: i32 = Porffor.wasm.i32.load(ptr, 0, 0) + 1;
+  // increment size
+  Porffor.wasm.i32.store(ptr, size, 0, 0); 
+  // store description ptr
+  Porffor.wasm.i32.store(ptr + size * 4, value, 0, 0);
+  
+  return size;
+}
 
-  if (op) { // write
-    const size: number = Porffor.wasm.i32.load(ptr, 0, 0);
-    Porffor.wasm.i32.store(ptr, size + 1, 0, 0)
-
-    // reuse set internals to store description
-    Porffor.set.write(ptr, size, value);
-    return size;
-  } else { // read
-    return Porffor.set.read(ptr, value);
-  }
-};
+export const __Porffor_symbol_readDesc = (sym: any): bytestring => {
+  const ptr: number = Porffor.allocateNamedPage<any>("symbol");
+  const desc: bytestring = Porffor.wasm.i32.load(ptr + sym * 4, 0, 4);
+  return desc;
+}
 
 export const Symbol = (description: any): Symbol => {
-  // 1-based so always truthy as numeric value
-  const symPtr: Symbol = __Porffor_symbol_descStore(true, description) + 1;
+  if (Porffor.rawType(description) == Porffor.TYPES.undefined) {
+    const symPtr: Symbol = __Porffor_symbol_create('');
+    return symPtr;
+  }
+  const symPtr: Symbol = __Porffor_symbol_create(description);
   return symPtr;
 };
 
 export const __Symbol_prototype_description$get = (_this: Symbol) => {
-  const description: bytestring =
-    __Porffor_symbol_descStore(false, Porffor.wasm`local.get ${_this}` - 1);
+  const description: bytestring = __Porffor_symbol_readDesc(_this);
   return description;
 };
 
 export const __Symbol_prototype_toString = (_this: Symbol) => {
-  const description: bytestring = __Porffor_symbol_descStore(false, Porffor.wasm`local.get ${_this}` - 1);
+  const description: bytestring =  __Porffor_symbol_readDesc(_this);
   const descLen: i32 = description.length;
   
   let out = Porffor.allocateBytes<bytestring>(4 + 8 + descLen);
