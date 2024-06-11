8a4e5fdc327931650321ffc85ddbc224691291a5
CanadaHonk <honk@goose.icu>
2024-06-11 16:24:12 +0100

    builtins/arraybuffer: impl slice
    test262: 18.30% (+0.01) | 🧪 50032 | 🤠 9156 (+7) | ❌ 3022 (+4) | 💀 19506 (-10) | 🏗️ 483 | 💥 425 | ⏰ 6 (-1) | 📝 17434

--- compiler/builtins/arraybuffer.ts
+++ compiler/builtins/arraybuffer.ts
@@ -22,3 +22,53 @@ export const ArrayBuffer = function (length: number): ArrayBuffer {
 export const __ArrayBuffer_prototype_byteLength$get = (_this: ArrayBuffer) => {
   return Porffor.wasm.i32.load(_this, 0, 0);
 };
+
+export const __ArrayBuffer_prototype_slice = (_this: ArrayBuffer, start: number, end: any) => {
+  const len: i32 = Porffor.wasm.i32.load(_this, 0, 0);
+
+  if (Porffor.rawType(end) == Porffor.TYPES.undefined) end = len;
+
+  start |= 0;
+  end |= 0;
+
+  if (start < 0) {
+    start = len + start;
+    if (start < 0) start = 0;
+  }
+  if (start > len) start = len;
+  if (end < 0) {
+    end = len + end;
+    if (end < 0) end = 0;
+  }
+  if (end > len) end = len;
+
+  const out: ArrayBuffer = Porffor.allocateBytes(4 + (end - start));
+  Porffor.wasm.i32.store(out, end - start, 0, 0);
+
+  Porffor.wasm`
+;; dst = out + 4
+local.get ${out}
+i32.to_u
+i32.const 4
+i32.add
+
+;; src = this + 4 + start
+local.get ${_this}
+i32.to_u
+i32.const 4
+i32.add
+local.get ${start}
+i32.to_u
+i32.add
+
+;; size = end - start
+local.get ${end}
+i32.to_u
+local.get ${start}
+i32.to_u
+i32.sub
+
+memory.copy 0 0`;
+
+  return out;
+};
