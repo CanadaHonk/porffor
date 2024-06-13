1d8b008d180964b7b899613955525b19f818ede3
CanadaHonk <honk@goose.icu>
2024-06-13 23:05:14 +0100

    builtins/annexb_string: impl arg methods
    test262: 18.94% (+0.01) | 🧪 50032 | 🤠 9474 (+4) | ❌ 3029 | 💀 19119 | 🏗️ 487 | 💥 425 | ⏰ 6 | 📝 17492 (-4)

--- compiler/builtins/annexb_string.js
+++ compiler/builtins/annexb_string.js
@@ -7,7 +7,6 @@ export const __String_prototype_${a0} = (_this: string) => {
   let out: string = Porffor.s\`<${a1}>\`;
 
   let outPtr: i32 = Porffor.wasm\`local.get \${out}\` + ${(2 + a1.length) * 2};
-
   let thisPtr: i32 = Porffor.wasm\`local.get \${_this}\`;
   let thisLen: i32 = _this.length;
   let endPtr: i32 = thisPtr + thisLen * 2;
@@ -35,7 +34,6 @@ export const __ByteString_prototype_${a0} = (_this: bytestring) => {
   let out: bytestring = Porffor.bs\`<${a1}>\`;
 
   let outPtr: i32 = Porffor.wasm\`local.get \${out}\` + ${2 + a1.length};
-
   let thisPtr: i32 = Porffor.wasm\`local.get \${_this}\`;
   let thisLen: i32 = _this.length;
   let endPtr: i32 = thisPtr + thisLen;
@@ -67,6 +65,77 @@ ${[...a1].map((x, i) => `  Porffor.wasm.i32.store8(outPtr, ${x.charCodeAt(0)}, 0
   noArgs('strike', 'strike');
   noArgs('sub', 'sub');
   noArgs('sup', 'sup');
+
+  const arg = (name, s1, s2) => out += `
+export const __String_prototype_${name} = (_this: string, arg: any) => {
+  let out: string = Porffor.s\`${`<${s1} ${s2}="`}\`;
+  out += arg;
+
+  let outPtr: i32 = Porffor.wasm\`local.get \${out}\` + ${(`<${s1} ${s2}="`.length + 2) * 2} + arg.length * 2;
+
+  Porffor.wasm.i32.store16(outPtr, 34, 0, 0); // "
+  Porffor.wasm.i32.store16(outPtr, 62, 0, 2); // >
+
+  let thisPtr: i32 = Porffor.wasm\`local.get \${_this}\`;
+  let thisLen: i32 = _this.length;
+  let endPtr: i32 = thisPtr + thisLen * 2;
+
+  while (thisPtr < endPtr) {
+    let chr: i32 = Porffor.wasm.i32.load16_u(thisPtr, 0, 4);
+    Porffor.wasm.i32.store16(outPtr, chr, 0, 4);
+
+    thisPtr += 2;
+    outPtr += 2;
+  }
+
+  Porffor.wasm.i32.store16(outPtr, 60, 0, 4); // <
+  Porffor.wasm.i32.store16(outPtr, 47, 0, 6); // /
+
+${[...s1].map((x, i) => `  Porffor.wasm.i32.store16(outPtr, ${x.charCodeAt(0)}, 0, ${8 + i * 2}); // ${x}`).join('\n')}
+
+  Porffor.wasm.i32.store16(outPtr, 62, 0, ${8 + s1.length * 2}); // >
+
+  out.length = thisLen + arg.length + ${`<${s1} ${s2}="`.length + s1.length + 5};
+
+  return out;
+};
+export const __ByteString_prototype_${name} = (_this: bytestring, arg: any) => {
+  let out: string = Porffor.s\`${`<${s1} ${s2}="`}\`;
+  out += arg;
+
+  let outPtr: i32 = Porffor.wasm\`local.get \${out}\` + ${(`<${s1} ${s2}="`.length + 2) * 2} + arg.length * 2;
+
+  Porffor.wasm.i32.store16(outPtr, 34, 0, 0); // "
+  Porffor.wasm.i32.store16(outPtr, 62, 0, 2); // >
+
+  let thisPtr: i32 = Porffor.wasm\`local.get \${_this}\`;
+  let thisLen: i32 = _this.length;
+  let endPtr: i32 = thisPtr + thisLen;
+
+  while (thisPtr < endPtr) {
+    let chr: i32 = Porffor.wasm.i32.load8_u(thisPtr++, 0, 4);
+    Porffor.wasm.i32.store16(outPtr, chr, 0, 4);
+
+    outPtr += 2;
+  }
+
+  Porffor.wasm.i32.store16(outPtr, 60, 0, 4); // <
+  Porffor.wasm.i32.store16(outPtr, 47, 0, 6); // /
+
+${[...s1].map((x, i) => `  Porffor.wasm.i32.store16(outPtr, ${x.charCodeAt(0)}, 0, ${8 + i * 2}); // ${x}`).join('\n')}
+
+  Porffor.wasm.i32.store16(outPtr, 62, 0, ${8 + s1.length * 2}); // >
+
+  out.length = thisLen + arg.length + ${`<${s1} ${s2}="`.length + s1.length + 5};
+
+  return out;
+};
+`;
+
+  arg('fontcolor', 'font', 'color');
+  arg('fontsize', 'font', 'size');
+  arg('anchor', 'a', 'name');
+  arg('link', 'a', 'href');
 
   return out;
 };
--- runner/index.js
+++ runner/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import fs from 'node:fs';
-globalThis.version = '0.18.36+ce8d0e379';
+globalThis.version = '0.18.37+16950ed91';
 
 // deno compat
 if (typeof process === 'undefined' && typeof Deno !== 'undefined') {
