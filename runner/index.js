8d284d25e2e046bf51a30bb816e32c25233c8ab5
CanadaHonk <honk@goose.icu>
2024-06-17 21:29:57 +0100

    site: add big total test262%

--- runner/index.js
+++ runner/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import fs from 'node:fs';
-globalThis.version = '0.19.7+120636c7c';
+globalThis.version = '0.19.8+92c5cf593';
 
 // deno compat
 if (typeof process === 'undefined' && typeof Deno !== 'undefined') {
