5979b079a2ed760d0fa752a63ca7f62a1cec7d7d
CanadaHonk <honk@goose.icu>
2024-06-21 00:01:02 +0100

    builtins/object: impl Object.values

--- compiler/builtins/object.ts
+++ compiler/builtins/object.ts
@@ -23,6 +23,29 @@ export const __Object_keys = (obj: any): any[] => {
   return out;
 };
 
+export const __Object_values = (obj: any): any[] => {
+  const out: any[] = Porffor.allocate();
+
+  const t: i32 = Porffor.rawType(obj);
+  if (t == Porffor.TYPES.object) {
+    const size: i32 = Porffor.wasm.i32.load(Porffor.wasm.i32.load(obj, 0, 0), 0, 0);
+    const vals: any[] = Porffor.wasm.i32.load(obj, 0, 4);
+    for (let i: i32 = 0; i < size; i++) {
+      Porffor.fastPush(out, vals[i]);
+    }
+  } else if (Porffor.fastOr(
+    t == Porffor.TYPES.array,
+    t == Porffor.TYPES.bytestring,
+    t == Porffor.TYPES.string
+  )) {
+    for (const x of obj) {
+      Porffor.fastPush(out, x);
+    }
+  }
+
+  return out;
+};
+
 export const __Object_prototype_toString = (_this: object) => {
   let out: bytestring = '[object Object]';
   return out;
