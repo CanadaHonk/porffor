f63a809344305c54a0044b4f4364f70d8e907268
CanadaHonk <honk@goose.icu>
2024-06-20 01:39:09 +0100

    builtins/string: use ecma262.

--- compiler/builtins/string_f64.ts
+++ compiler/builtins/string_f64.ts
@@ -4,7 +4,7 @@ import type {} from './porffor.d.ts';
 // todo: support constructor/string objects properly
 export const String = function (value: any): bytestring {
   if (!new.target && Porffor.rawType(value) == Porffor.TYPES.symbol) return __Symbol_prototype_toString(value);
-  return __ecma262_ToString(value);
+  return ecma262.ToString(value);
 };
 
 export const __String_fromCharCode = (...codes: any[]): bytestring|string => {
