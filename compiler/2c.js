979a14a7772ee19f498a63a94fc625e0d10f617f
CanadaHonk <honk@goose.icu>
2024-06-20 02:00:22 +0100

    2c: log func name in unimplemented warning

--- compiler/2c.js
+++ compiler/2c.js
@@ -790,7 +790,7 @@ _time_out = _time.tv_nsec / 1000000. + _time.tv_sec * 1000.;`);
             break;
           }
 
-          log.warning('2c', `unimplemented op: ${invOpcodes[i[0]]}`);
+          log.warning('2c', `unimplemented op: ${invOpcodes[i[0]]} \x1b[90m(${f.name})`);
       }
 
       lastCond = false;
