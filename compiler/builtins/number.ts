4494fb7fac5fd97210423a2ec1da507d811f79c3
CanadaHonk <honk@goose.icu>
2024-07-01 23:24:46 +0100

    builtins/number: cleanup parseInt

--- compiler/builtins/number.ts
+++ compiler/builtins/number.ts
@@ -543,22 +543,22 @@ export const parseInt = (input: any, radix: any): f64 => {
   }
   if (radix < 2 || radix > 36) return NaN;
 
-  let nMax: f64 = 58;
+  let nMax: i32 = 58;
   if (radix < 10) nMax = 48 + radix;
 
   let n: f64 = NaN;
 
-  const inputPtr: f64 = Porffor.wasm`local.get ${input}`;
-  const len: f64 = Porffor.wasm.i32.load(inputPtr, 0, 0);
-  let i: f64 = inputPtr;
+  const inputPtr: i32 = Porffor.wasm`local.get ${input}`;
+  const len: i32 = Porffor.wasm.i32.load(inputPtr, 0, 0);
+  let i: i32 = inputPtr;
 
   let negative: boolean = false;
 
   if (Porffor.rawType(input) == Porffor.TYPES.bytestring) {
-    const endPtr: f64 = i + len;
+    const endPtr: i32 = i + len;
 
     // check start of string
-    const startChr: f64 = Porffor.wasm.i32.load8_u(i, 0, 4);
+    const startChr: i32 = Porffor.wasm.i32.load8_u(i, 0, 4);
 
     // +, ignore
     if (startChr == 43) i++;
@@ -571,7 +571,7 @@ export const parseInt = (input: any, radix: any): f64 => {
 
     // 0, potential start of hex
     if ((defaultRadix || radix == 16) && startChr == 48) {
-      const second: f64 = Porffor.wasm.i32.load8_u(i + 1, 0, 4);
+      const second: i32 = Porffor.wasm.i32.load8_u(i + 1, 0, 4);
       // 0x or 0X
       if (second == 120 || second == 88) {
         // set radix to 16 and skip leading 2 chars
@@ -581,24 +581,18 @@ export const parseInt = (input: any, radix: any): f64 => {
     }
 
     while (i < endPtr) {
-      const chr: f64 = Porffor.wasm.i32.load8_u(i++, 0, 4);
+      const chr: i32 = Porffor.wasm.i32.load8_u(i++, 0, 4);
 
       if (chr >= 48 && chr < nMax) {
         if (Number.isNaN(n)) n = 0;
-
-        n *= radix;
-        n += chr - 48;
+        n = (n * radix) + chr - 48;
       } else if (radix > 10) {
         if (chr >= 97 && chr < (87 + radix)) {
           if (Number.isNaN(n)) n = 0;
-
-          n *= radix;
-          n += chr - 87;
+          n = (n * radix) + chr - 87;
         } else if (chr >= 65 && chr < (55 + radix)) {
           if (Number.isNaN(n)) n = 0;
-
-          n *= radix;
-          n += chr - 55;
+          n = (n * radix) + chr - 55;
         } else {
           break;
         }
@@ -611,10 +605,10 @@ export const parseInt = (input: any, radix: any): f64 => {
     return n;
   }
 
-  const endPtr: f64 = i + len * 2;
+  const endPtr: i32 = i + len * 2;
 
   // check start of string
-  const startChr: f64 = Porffor.wasm.i32.load16_u(i, 0, 4);
+  const startChr: i32 = Porffor.wasm.i32.load16_u(i, 0, 4);
 
   // +, ignore
   if (startChr == 43) i += 2;
@@ -627,7 +621,7 @@ export const parseInt = (input: any, radix: any): f64 => {
 
   // 0, potential start of hex
   if ((defaultRadix || radix == 16) && startChr == 48) {
-    const second: f64 = Porffor.wasm.i32.load16_u(i + 2, 0, 4);
+    const second: i32 = Porffor.wasm.i32.load16_u(i + 2, 0, 4);
     // 0x or 0X
     if (second == 120 || second == 88) {
       // set radix to 16 and skip leading 2 chars
@@ -637,25 +631,19 @@ export const parseInt = (input: any, radix: any): f64 => {
   }
 
   while (i < endPtr) {
-    const chr: f64 = Porffor.wasm.i32.load16_u(i, 0, 4);
+    const chr: i32 = Porffor.wasm.i32.load16_u(i, 0, 4);
     i += 2;
 
     if (chr >= 48 && chr < nMax) {
       if (Number.isNaN(n)) n = 0;
-
-      n *= radix;
-      n += chr - 48;
+      n = (n * radix) + chr - 48;
     } else if (radix > 10) {
       if (chr >= 97 && chr < (87 + radix)) {
         if (Number.isNaN(n)) n = 0;
-
-        n *= radix;
-        n += chr - 87;
+        n = (n * radix) + chr - 87;
       } else if (chr >= 65 && chr < (55 + radix)) {
         if (Number.isNaN(n)) n = 0;
-
-        n *= radix;
-        n += chr - 55;
+        n = (n * radix) + chr - 55;
       } else {
         break;
       }
