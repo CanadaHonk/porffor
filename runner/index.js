12c74dc58d0bdf1a2d59e90a26ab85476d2eab28
CanadaHonk <honk@goose.icu>
2024-07-02 20:59:40 +0100

    builtins/object: more compliant toString and more
    test262 goes slightly down due to `this` not working properly.
    
    test262: 25.00% (-0.01) | 🧪 49471 | 🤠 12366 (-7) | ❌ 3617 (-16) | 💀 21058 (+23) | 🏗 432 | 💥 311 | ⏰ 569 | 📝 11118

--- runner/index.js
+++ runner/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import fs from 'node:fs';
-globalThis.version = '0.24.11+a277981f6';
+globalThis.version = '0.24.12+276aa969c';
 
 // deno compat
 if (typeof process === 'undefined' && typeof Deno !== 'undefined') {
