50d0fb067db77e212097651b50b4e6359ddc7988
CanadaHonk <honk@goose.icu>
2024-07-02 14:09:57 +0100

    site: revamp

--- runner/index.js
+++ runner/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import fs from 'node:fs';
-globalThis.version = '0.24.7+23de87c30';
+globalThis.version = '0.24.8+02241f0c9';
 
 // deno compat
 if (typeof process === 'undefined' && typeof Deno !== 'undefined') {
