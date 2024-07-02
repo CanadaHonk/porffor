b04820200cdf8b70974d31e212533cd28edb16e1
CanadaHonk <honk@goose.icu>
2024-07-02 14:20:41 +0100

    site: tweaks

--- runner/index.js
+++ runner/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import fs from 'node:fs';
-globalThis.version = '0.24.8+02241f0c9';
+globalThis.version = '0.24.9+fcb5b03c7';
 
 // deno compat
 if (typeof process === 'undefined' && typeof Deno !== 'undefined') {
