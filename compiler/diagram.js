947cd286e2fc2684d96e3d08181f85b6e5b3d141
CanadaHonk <honk@goose.icu>
2024-07-07 16:31:15 +0100

    diagram: initial add

---
+++ compiler/diagram.js
@@ -0,0 +1,31 @@
+export const tree = data => {
+  let out = 'flowchart LR\n';
+  let ids = new Map();
+
+  const run = (x, parent = null) => {
+    for (const [ name, children ] of x) {
+      const alreadyHas = ids.has(name);
+      if (!alreadyHas) ids.set(name, ids.size);
+      const id = ids.get(name);
+
+      if (!alreadyHas || parent != null) {
+        if (parent != null) out += `${parent}-->`;
+        out += `${id}${alreadyHas ? '' : `["${name}"]`}\n`;
+      }
+
+      if (children) run(children, id);
+    }
+  };
+  run(data);
+
+  return out;
+};
+
+export const url = code => `https://mermaid.live/view#${btoa(JSON.stringify({
+  mermaid: { theme: 'dark', securityLevel: 'loose', maxEdges: 5000 },
+  updateDiagram: true,
+  autoSync: true,
+  rough: false,
+  panZoom: true,
+  code
+}))}`;
