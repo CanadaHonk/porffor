a2cda7068e3152fdb251ea5bd5a0aa879b67dbea
Bob Varioa <true.email001@gmail.com>
2024-07-11 09:20:57 -0500

    codegen: fix destructuring defaults
    test262: 29.07% (+0.06) | 🧪 47471 | 🤠 13801 (+28) | ❌ 4965 (-28) | 💀 14829 | 🏗 429 | 💥 510 | ⏰ 13 | 📝 12924

--- compiler/codegen.js
+++ compiler/codegen.js
@@ -3023,16 +3023,12 @@ const generateVar = (scope, decl) => {
               type: 'VariableDeclarator',
               id: e.left,
               init: {
-                type: 'LogicalExpression',
-                operator: '??',
-                left: {
-                  type: 'MemberExpression',
-                  object: { type: 'Identifier', name: tmpName },
-                  property: { type: 'Literal', value: i },
-                  computed: true
-                },
-                right: e.right
-              }
+                type: 'MemberExpression',
+                object: { type: 'Identifier', name: tmpName },
+                property: { type: 'Literal', value: i },
+                computed: true
+              },
+              _default: e.right
             });
 
             break;
@@ -3065,7 +3061,8 @@ const generateVar = (scope, decl) => {
           declarations: [{
             type: 'VariableDeclarator',
             id: { type: 'Identifier', name: tmpName },
-            init: x.init
+            init: x.init,
+            _default: x._default
           }],
           kind: decl.kind,
           _global: false
@@ -3110,16 +3107,12 @@ const generateVar = (scope, decl) => {
               type: 'VariableDeclarator',
               id: prop.value.left,
               init: {
-                type: 'LogicalExpression',
-                operator: '??',
-                left: {
-                  type: 'MemberExpression',
-                  object: { type: 'Identifier', name: tmpName },
-                  property: prop.key,
-                  computed: prop.computed
-                },
-                right: prop.value.right
-              }
+                type: 'MemberExpression',
+                object: { type: 'Identifier', name: tmpName },
+                property: prop.key,
+                computed: prop.computed
+              },
+              _default: prop.value.right
             });
           } else {
             decls.push({
@@ -3144,7 +3137,8 @@ const generateVar = (scope, decl) => {
           declarations: [{
             type: 'VariableDeclarator',
             id: { type: 'Identifier', name: tmpName },
-            init: x.init
+            init: x.init,
+            _default: x._default
           }],
           kind: decl.kind,
           _global: false
@@ -3225,6 +3219,17 @@ const generateVar = (scope, decl) => {
       }
 
       out = out.concat(newOut);
+
+      if (x._default) {
+        out.push(
+          ...typeIsOneOf(getType(scope, name), [ TYPES.undefined, TYPES.empty ]),
+          [ Opcodes.if, Blocktype.void ],
+            ...generate(scope, x._default, global, name),
+            [ global ? Opcodes.global_set : Opcodes.local_set, idx ],
+            ...setType(scope, name, getNodeType(scope, x._default)),
+          [ Opcodes.end ],
+        );
+      }
 
       if (globalThis.precompile && global) {
         scope.globalInits ??= {};
