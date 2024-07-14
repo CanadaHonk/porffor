2b3aca2e3226c11097df54482c224aed7e58511f
CanadaHonk <honk@goose.icu>
2024-07-14 22:14:00 +0100

    version: bump to 0.30

--- runner/index.js
+++ runner/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import fs from 'node:fs';
-globalThis.version = '0.28.14+a2b93aee9';
+globalThis.version = '0.30.0+b1785d63e';
 
 // deno compat
 if (typeof process === 'undefined' && typeof Deno !== 'undefined') {
