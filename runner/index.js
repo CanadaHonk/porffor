7364f571eaf2d877feb570609dca6283a60bee6d
Oliver Medhurst <honk@goose.icu>
2024-07-30 20:16:12 +0100

    pr: slightly more serious now

--- runner/index.js
+++ runner/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import fs from 'node:fs';
-globalThis.version = '0.34.11+f7ca044b2';
+globalThis.version = '0.34.12+a590b1020';
 
 // deno compat
 if (typeof process === 'undefined' && typeof Deno !== 'undefined') {
