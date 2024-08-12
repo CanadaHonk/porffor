3d0ff08b878aa5f409f0faeba08b983dea8138cb
Oliver Medhurst <honk@goose.icu>
2024-08-12 10:28:14 +0100

    test262/harness: more typedarray harness
    test262: 37.69% | 🧪 48373 | 🤠 18232 (+2) | ❌ 5754 (+11) | 💀 17487 (-13) | 🏗 263 | 💥 464 | ⏰ 13 | 📝 6160

--- runner/index.js
+++ runner/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import fs from 'node:fs';
-globalThis.version = '0.37.5+c797bd00d';
+globalThis.version = '0.37.6+834657df0';
 
 // deno compat
 if (typeof process === 'undefined' && typeof Deno !== 'undefined') {
