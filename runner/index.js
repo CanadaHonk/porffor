34754f21f8c669f4120718c3ee2aa9f4a5174f05
Oliver Medhurst <honk@goose.icu>
2024-08-15 14:15:07 +0100

    builtins/precompiled: rebuild

--- runner/index.js
+++ runner/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import fs from 'node:fs';
-globalThis.version = '0.37.16+2b7968825';
+globalThis.version = '0.37.17+5b3ce1ba8';
 
 // deno compat
 if (typeof process === 'undefined' && typeof Deno !== 'undefined') {
