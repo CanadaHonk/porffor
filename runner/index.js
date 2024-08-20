727ebc52f1d27f308504953397ae5da2fc44d67f
Oliver Medhurst <honk@goose.icu>
2024-08-20 19:51:08 +0100

    test262/harness: disable unneeded object value check for verifyProperty
    test262: 38.73% (+0.21) | 🧪 48373 | 🤠 18735 (+100) | ❌ 6012 | 💀 16758 (-98) | 🏗 264 | 💥 423 | ⏰ 15 (-2) | 📝 6166

--- runner/index.js
+++ runner/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import fs from 'node:fs';
-globalThis.version = '0.37.25+8558a306e';
+globalThis.version = '0.37.26+4f2db33d1';
 
 // deno compat
 if (typeof process === 'undefined' && typeof Deno !== 'undefined') {
