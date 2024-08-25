118aa8e06fd28c0a76c060e6c89f4a44cc5e3d83
Oliver Medhurst <honk@goose.icu>
2024-08-25 10:54:37 +0100

    version: bump to 0.39

--- runner/index.js
+++ runner/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import fs from 'node:fs';
-globalThis.version = '0.37.32+fde2c20c4';
+globalThis.version = '0.39.0+1c0b122a3';
 
 // deno compat
 if (typeof process === 'undefined' && typeof Deno !== 'undefined') {
