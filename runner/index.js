241df5afd03129e08204d271c5964fbd2713e3b4
Oliver Medhurst <honk@goose.icu>
2024-08-27 02:36:03 +0100

    test262: fix scripts for 4X%

--- runner/index.js
+++ runner/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import fs from 'node:fs';
-globalThis.version = '0.40.0+4d30ede66';
+globalThis.version = '0.40.1+5e139c5e1';
 
 // deno compat
 if (typeof process === 'undefined' && typeof Deno !== 'undefined') {
