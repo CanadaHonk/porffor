487fe44a34a2604788e6cc37995c88c3903b04da
Oliver Medhurst <honk@goose.icu>
2024-09-09 02:55:08 +0100

    version: bump to 0.42

--- runner/index.js
+++ runner/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import fs from 'node:fs';
-globalThis.version = '0.41.7+89be416c1';
+globalThis.version = '0.42.0+93da2c806';
 
 // deno compat
 if (typeof process === 'undefined' && typeof Deno !== 'undefined') {
