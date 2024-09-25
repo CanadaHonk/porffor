545a6e92f4e8f503734c2c26fa32629cd6bae808
Oliver Medhurst <honk@goose.icu>
2024-09-25 15:24:06 +0100

    publish: disable rev in version number

--- publish.js
+++ publish.js
@@ -1,15 +1,15 @@
 import { execSync } from 'node:child_process';
 import fs from 'node:fs';
 
-const rev = fs.readFileSync('.git/refs/heads/main', 'utf8').trim().slice(0, 9);
-
 const packageJson = fs.readFileSync('package.json', 'utf8');
 const packageVersion = JSON.parse(packageJson).version;
 
 const majorminor = packageVersion.split('.').slice(0, 2).join('.');
 const patch = parseInt(packageVersion.split('.')[2].split(/[^0-9\-]/)[0]) + 1;
 
-const version = `${majorminor}.${patch}+${rev}`;
+// const rev = fs.readFileSync('.git/refs/heads/main', 'utf8').trim().slice(0, 9);
+// const version = `${majorminor}.${patch}+${rev}`;
+const version = `${majorminor}.${patch}`;
 
 fs.writeFileSync('package.json', packageJson.replace(`"${packageVersion}"`, `"${version}"`));
 
--- runner/index.js
+++ runner/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import fs from 'node:fs';
-globalThis.version = '0.47.0+bfae421aa';
+globalThis.version = '0.47.1';
 
 // deno compat
 if (typeof process === 'undefined' && typeof Deno !== 'undefined') {
