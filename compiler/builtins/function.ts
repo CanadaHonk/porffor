7645b248f74010fc56f0534c76102b5c5f8a26f4
Oliver Medhurst <honk@goose.icu>
2024-10-12 10:45:14 +0900

    builtins/function: more compliant toString

--- compiler/builtins/function.ts
+++ compiler/builtins/function.ts
@@ -1,7 +1,16 @@
 import type {} from './porffor.d.ts';
 
 export const __Function_prototype_toString = (_this: Function) => {
-  // todo: actually use source
-  let out: bytestring = 'function () {}';
+  const out: bytestring = Porffor.allocate();
+
+  const prefix: bytestring = 'function ';
+  Porffor.bytestring.appendStr(out, prefix);
+
+  Porffor.bytestring.appendStr(out, _this.name);
+
+  const postfix: bytestring = '() { [native code] }';
+  Porffor.bytestring.appendStr(out, postfix);
   return out;
 };
+
+export const __Function_prototype_toLocaleString = (_this: Function) => __Function_prototype_toString(_this);
