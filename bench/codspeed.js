b023571631e65a87d697dd454fc95d4cc6226de1
Oliver Medhurst <honk@goose.icu>
2024-11-24 23:05:30 +0000

    codspeed: simplify

--- bench/codspeed.js
+++ bench/codspeed.js
@@ -6,25 +6,16 @@ const bench = withCodSpeed(new Bench());
 import { readFileSync } from 'node:fs';
 import compile from '../compiler/wrap.js';
 
-const argv = process.argv.slice();
-
 const files = [ 'richards.js' ];
-const prefVariants = [ '', '-O0', '--no-coctc' ];
 for (const file of files) {
-  for (const prefs of prefVariants) {
-    let run;
-    bench.add((file + ' ' + prefs).trim(), () => {
-      run();
-    }, {
-      beforeEach: () => {
-        // fresh compile for each run
-        process.argv = (argv.join(' ') + ' ' + prefs).trim().split(' ');
-        globalThis.argvChanged();
-
-        run = compile(readFileSync('bench/' + file, 'utf8')).exports.main;
-      }
-    });
-  }
+  let run;
+  bench.add(file, () => {
+    run();
+  }, {
+    beforeEach: () => {
+      run = compile(readFileSync('bench/' + file, 'utf8')).exports.main;
+    }
+  });
 }
 
 await bench.run();
