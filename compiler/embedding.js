cc5012173c83009c44253c3c62aaf93d2cd3f402
Oliver Medhurst <honk@goose.icu>
2024-12-07 20:34:08 +0000

    embedding: delete unused code

--- compiler/embedding.js
+++ compiler/embedding.js
@@ -9,16 +9,3 @@ export const number = (n, valtype = valtypeBinary) => {
 
   return out;
 };
-
-export const enforceOneByte = arr => [ arr[0] ?? 0 ];
-export const enforceTwoBytes = arr => [ arr[0] ?? 0, arr[1] ?? 0 ];
-export const enforceFourBytes = arr => [ arr[0] ?? 0, arr[1] ?? 0, arr[2] ?? 0, arr[3] ?? 0 ];
-export const enforceEightBytes = arr => [ arr[0] ?? 0, arr[1] ?? 0, arr[2] ?? 0, arr[3] ?? 0, arr[4] ?? 0, arr[5] ?? 0, arr[6] ?? 0, arr[7] ?? 0 ];
-
-export const i32x4 = (a, b, c, d) => [ [
-  ...Opcodes.v128_const,
-  ...enforceFourBytes(signedLEB128(a)),
-  ...enforceFourBytes(signedLEB128(b)),
-  ...enforceFourBytes(signedLEB128(c)),
-  ...enforceFourBytes(signedLEB128(d))
-] ];
