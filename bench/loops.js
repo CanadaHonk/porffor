52a3ce5cac2668fa4e22dfe12f8a0ac962c3e3a6
Oliver Medhurst <honk@goose.icu>
2025-01-06 23:57:50 +0000

    bench/loops: nicer logs

--- bench/loops.js
+++ bench/loops.js
@@ -5,14 +5,14 @@ let sum1 = 0;
 for (let x of data) {
   sum1 += x;
 }
-console.log(performance.now() - t1);
+console.log('for..of', performance.now() - t1);
 
 let t2 = performance.now();
 let sum2 = 0;
 for (let i = 0; i < data.length; i++) {
   sum2 += data[i];
 }
-console.log(performance.now() - t2);
+console.log('for (let i = 0; i < data.length; i++)', performance.now() - t2);
 
 let t3 = performance.now();
 let sum3 = 0;
@@ -20,4 +20,4 @@ let len = data.length;
 for (let i = 0; i < len; i++) {
   sum3 += data[i];
 }
-console.log(performance.now() - t3);
+console.log('for (let i = 0; i < len; i++)', performance.now() - t3);
