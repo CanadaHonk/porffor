34067c62168febc8ece137b6e5ccd41fb47c3ed6
Oliver Medhurst <honk@goose.icu>
2025-02-09 01:48:15 +0000

    bench/avg: logs

--- bench/avg.js
+++ bench/avg.js
@@ -6,11 +6,13 @@ const cmd = process.argv[3];
 
 const noAnsi = s => s.replace(/\u001b\[[0-9]+m/g, '');
 
+let spinner = ['-', '\\', '|', '/'], spin = 0;
+
 let data = [];
 for (let i = 0; i < times; i++) {
+  process.stdout.write('\r         \r' + spinner[spin++ % spinner.length] + ' ' + (i + 1));
   data.push(parseFloat(noAnsi(execSync(cmd, { stdio: 'pipe' }).toString().trim())));
-
   await new Promise(res => setTimeout(res, 100));
 }
 
-console.log((data.reduce((acc, x) => acc + x, 0) / data.length).toFixed(0));
+console.log('\r        \r' + (data.reduce((acc, x) => acc + x, 0) / data.length).toFixed(0));
