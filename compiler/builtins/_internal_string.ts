cde4cef2d9b8656c78ba698208381d3fd894f324
Oliver Medhurst <honk@goose.icu>
2025-03-08 03:16:37 -0800

    builtins/internal_string: cleanup

--- compiler/builtins/_internal_string.ts
+++ compiler/builtins/_internal_string.ts
@@ -1,34 +1,29 @@
 import type {} from './porffor.d.ts';
 
 export const __Porffor_compareStrings = (a: any, b: any): boolean => {
-  let at: i32 = Porffor.type(a);
-  let bt: i32 = Porffor.type(b);
-
-  if ((at | 0b10000000) != Porffor.TYPES.bytestring) {
+  if ((Porffor.type(a) | 0b10000000) != Porffor.TYPES.bytestring) {
     // a is not string or bytestring
     // check if it is bad type or value
     if (Porffor.fastOr(
       a == null,
 
-      at == Porffor.TYPES.symbol,
-      at == Porffor.TYPES.boolean
+      Porffor.type(a) == Porffor.TYPES.symbol,
+      Porffor.type(a) == Porffor.TYPES.boolean
     )) return false;
 
-    // todo/perf: just use a.toString()?
     a = ecma262.ToString(a);
   }
 
-  if ((bt | 0b10000000) != Porffor.TYPES.bytestring) {
+  if ((Porffor.type(b) | 0b10000000) != Porffor.TYPES.bytestring) {
     // b is not string or bytestring
     // check if it is bad type or value
     if (Porffor.fastOr(
       b == null,
 
-      bt == Porffor.TYPES.symbol,
-      bt == Porffor.TYPES.boolean
+      Porffor.type(b) == Porffor.TYPES.symbol,
+      Porffor.type(b) == Porffor.TYPES.boolean
     )) return false;
 
-    // todo/perf: just use b.toString()?
     b = ecma262.ToString(b);
   }
 
@@ -36,18 +31,13 @@ export const __Porffor_compareStrings = (a: any, b: any): boolean => {
 };
 
 export const __Porffor_concatStrings = (a: any, b: any): any => {
-  let at: i32 = Porffor.type(a);
-  let bt: i32 = Porffor.type(b);
-
-  if ((at | 0b10000000) != Porffor.TYPES.bytestring) {
+  if ((Porffor.type(a) | 0b10000000) != Porffor.TYPES.bytestring) {
     // a is not string or bytestring
-    // todo/perf: just use a.toString()?
     a = ecma262.ToString(a);
   }
 
-  if ((bt | 0b10000000) != Porffor.TYPES.bytestring) {
+  if ((Porffor.type(b) | 0b10000000) != Porffor.TYPES.bytestring) {
     // b is not string or bytestring
-    // todo/perf: just use b.toString()?
     b = ecma262.ToString(b);
   }
 
