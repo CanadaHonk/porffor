4c4bf8adda621c4b8f35bdd56765c57d6e130e65
Oliver Medhurst <honk@goose.icu>
2025-03-21 15:07:39 +0000

    codegen: optimize type usage
    - do not mark types as used if just used inside of built-ins and not returned to userland
    - mark types of type annotations of arguments
    - do not mark array for internal rest args arrays
    
    makes hello world ~10x smaller Wasm, ~2x smaller native

--- runner/index.js
+++ runner/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import fs from 'node:fs';
-globalThis.version = '0.57.0';
+globalThis.version = '0.57.1';
 
 // deno compat
 if (typeof process === 'undefined' && typeof Deno !== 'undefined') {
