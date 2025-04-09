bfc6c4830f4fec70a33836a827105fc146203500
Oliver Medhurst <honk@goose.icu>
2025-04-10 00:40:37 +0100

    precompile: stub imports to fix crash

--- runtime/index.js
+++ runtime/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import fs from 'node:fs';
-globalThis.version = '0.57.10';
+globalThis.version = '0.57.11';
 
 // deno compat
 if (typeof process === 'undefined' && typeof Deno !== 'undefined') {
