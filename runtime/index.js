74677020df85bd2e361fd998c80501659b3a4eaa
Oliver Medhurst <honk@goose.icu>
2025-04-10 02:23:33 +0100

    test262: recalibrate and update clone
    test262 down as bump in e58ec6b439480605ce25024af42a8866e4ecfad7 was false positive due to a bug fixed in previous commits
    
    test262: 58.11% (-0.22) | 🧪 50422 (+2) | 🤠 29299 (-112) | ❌ 7513 (-2) | 💀 13301 (+116) | 🏗️ 37 | 💥 263 | ⏰ 9 | 📝 0

--- runtime/index.js
+++ runtime/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import fs from 'node:fs';
-globalThis.version = '0.57.11';
+globalThis.version = '0.57.12';
 
 // deno compat
 if (typeof process === 'undefined' && typeof Deno !== 'undefined') {
