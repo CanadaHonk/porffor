dd893d317cec749b5add4e60e558087a933e2d25
Oliver Medhurst <honk@goose.icu>
2025-05-21 02:20:35 +0100

    precompile: use eval to defer parsing
    makes startup for all compiles ~30ms faster! this also includes test262 bump from prs!
    
    test262: 58.55% (+0.15) | 🧪 50424 | 🤠 29523 (+73) | ❌ 7576 (-75) | 💀 13042 (+2) | 🏗️ 7 | 💥 262 | ⏰ 14 | 📝 0

--- runtime/index.js
+++ runtime/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import fs from 'node:fs';
-globalThis.version = '0.57.28';
+globalThis.version = '0.57.30';
 
 // deno compat
 if (typeof process === 'undefined' && typeof Deno !== 'undefined') {
