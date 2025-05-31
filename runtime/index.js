c3a3b3a5f124c52186abf02fa4a495ac5b754b09
Oliver Medhurst <honk@goose.icu>
2025-05-31 02:42:33 +0200

    version: bump to 0.58

--- runtime/index.js
+++ runtime/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import fs from 'node:fs';
-globalThis.version = '0.57.33';
+globalThis.version = '0.58.0';
 
 // deno compat
 if (typeof process === 'undefined' && typeof Deno !== 'undefined') {
