f65503d18ad29818129e3cc7bf4d18ec3c3edf5a
Oliver Medhurst <honk@goose.icu>
2025-06-02 12:33:32 +0200

    publish: start publishing to jsr

--- publish.js
+++ publish.js
@@ -2,6 +2,7 @@ import { execSync } from 'node:child_process';
 import fs from 'node:fs';
 
 const packageJson = fs.readFileSync('package.json', 'utf8');
+const jsrJson = fs.readFileSync('jsr.json', 'utf8');
 const packageVersion = JSON.parse(packageJson).version;
 
 const majorminor = packageVersion.split('.').slice(0, 2).join('.');
@@ -12,13 +13,13 @@ const patch = parseInt(packageVersion.split('.')[2].split(/[^0-9\-]/)[0]) + 1;
 const version = `${majorminor}.${patch}`;
 
 fs.writeFileSync('package.json', packageJson.replace(`"${packageVersion}"`, `"${version}"`));
+fs.writeFileSync('jsr.json', jsrJson.replace(`"${packageVersion}"`, `"${version}"`));
 
 fs.writeFileSync('runtime/index.js', fs.readFileSync('runtime/index.js', 'utf8')
   .replace(/globalThis\.version = '.*?';/, `globalThis.version = '${version}';`));
 
-execSync(`git add package.json runtime/index.js`, { stdio: 'inherit' });
-
-// execSync(`git commit -m "version: ${version}"`, { stdio: 'inherit' });
+execSync(`git add package.json  jsr.json runtime/index.js`, { stdio: 'inherit' });
 execSync(`git commit --amend -C HEAD --no-verify`, { stdio: 'inherit' });
 
 execSync(`${process.env.NPM_BINARY ?? 'npm'} publish`, { stdio: 'inherit' });
+execSync(`${process.env.NPM_BINARY ?? 'npm'} x jsr -- publish --allow-dirty`, { stdio: 'inherit' });
