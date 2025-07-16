3e0ba85885ce9c328945b76fa2671869ac46d18d
Oliver Medhurst <honk@goose.icu>
2025-07-16 05:50:12 +0100

    test262/harness: impl regExpUtils.js
    test262: 60.52% (+0.08) | 🧪 50594 | 🤠 30619 (+39) | ❌ 7498 (+42) | 💀 12221 (-81) | 🏗️ 6 | 💥 232 | ⏰ 18 | 📝 0

--- test262/harness.js
+++ test262/harness.js
@@ -1517,6 +1517,103 @@ var __assert_compareIterator = (iter, validators) => {
   assert.sameValue(result.value, undefined);
 };
 
+/// regExpUtils.js
+function buildString(args) {
+  const loneCodePoints = args.loneCodePoints;
+  const ranges = args.ranges;
+  let result = String.fromCodePoint(...loneCodePoints);
+  for (let i = 0; i < ranges.length; i++) {
+    let range = ranges[i];
+    let start = range[0];
+    let end = range[1];
+    for (let codePoint = start; codePoint <= end; codePoint++) {
+      result += String.fromCodePoint(codePoint);
+    }
+  }
+  return result;
+}
+
+// function printCodePoint(codePoint) {
+//   const hex = codePoint
+//     .toString(16)
+//     .toUpperCase()
+//     .padStart(6, "0");
+//   return `U+${hex}`;
+// }
+
+// function printStringCodePoints(string) {
+//   const buf = [];
+//   for (let symbol of string) {
+//     let formatted = printCodePoint(symbol.codePointAt(0));
+//     buf.push(formatted);
+//   }
+//   return buf.join(' ');
+// }
+
+function testPropertyEscapes(regExp, string, expression) {
+  if (!regExp.test(string)) {
+    for (let symbol of string) {
+      // let formatted = printCodePoint(symbol.codePointAt(0));
+      assert(
+        regExp.test(symbol),
+        // `\`${ expression }\` should match ${ formatted } (\`${ symbol }\`)`
+      );
+    }
+  }
+}
+
+function testPropertyOfStrings(args) {
+  // Use member expressions rather than destructuring `args` for improved
+  // compatibility with engines that only implement assignment patterns
+  // partially or not at all.
+  const regExp = args.regExp;
+  const expression = args.expression;
+  const matchStrings = args.matchStrings;
+  const nonMatchStrings = args.nonMatchStrings;
+  const allStrings = matchStrings.join('');
+  if (!regExp.test(allStrings)) {
+    for (let string of matchStrings) {
+      assert(
+        regExp.test(string),
+        // `\`${ expression }\` should match ${ string } (${ printStringCodePoints(string) })`
+      );
+    }
+  }
+
+  if (!nonMatchStrings) return;
+
+  const allNonMatchStrings = nonMatchStrings.join('');
+  if (regExp.test(allNonMatchStrings)) {
+    for (let string of nonMatchStrings) {
+      assert(
+        !regExp.test(string),
+        // `\`${ expression }\` should not match ${ string } (${ printStringCodePoints(string) })`
+      );
+    }
+  }
+}
+
+// The exact same logic can be used to test extended character classes
+// as enabled through the RegExp `v` flag. This is useful to test not
+// just standalone properties of strings, but also string literals, and
+// set operations.
+const testExtendedCharacterClass = testPropertyOfStrings;
+
+// Returns a function that validates a RegExp match result.
+//
+// Example:
+//
+//    var validate = matchValidator(['b'], 1, 'abc');
+//    validate(/b/.exec('abc'));
+//
+function matchValidator(expectedEntries, expectedIndex, expectedInput) {
+  return function(match) {
+    assert.compareArray(match, expectedEntries, 'Match entries');
+    assert.sameValue(match.index, expectedIndex, 'Match index');
+    assert.sameValue(match.input, expectedInput, 'Match input');
+  }
+}
+
 /// sm/non262.js
 function print() {}
 function printBugNumber() {}
