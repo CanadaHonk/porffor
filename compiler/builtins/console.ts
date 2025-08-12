5815ca9c9bf9f0e590b1a3f6d64ad9d11ad2b788
Oliver Medhurst <honk@goose.icu>
2025-08-12 16:51:03 +0100

    builtins/console: print errors

--- compiler/builtins/console.ts
+++ compiler/builtins/console.ts
@@ -268,12 +268,33 @@ export const __Porffor_print = (arg: any, colors: boolean = true, depth: number 
       Porffor.printStatic('WeakRef {}');
       return;
 
-    // case Porffor.TYPES.regexp:
-    //   // todo: we currently have no way of getting the source text, so this falls back
-
-    // default:
-    //   __Porffor_printString(arg.toString());
-    //   return;
+    case Porffor.TYPES.error:
+      __Porffor_printString(__Error_prototype_toString(arg));
+      return;
+    case Porffor.TYPES.aggregateerror:
+      __Porffor_printString(__AggregateError_prototype_toString(arg));
+      return;
+    case Porffor.TYPES.typeerror:
+      __Porffor_printString(__TypeError_prototype_toString(arg));
+      return;
+    case Porffor.TYPES.referenceerror:
+      __Porffor_printString(__ReferenceError_prototype_toString(arg));
+      return;
+    case Porffor.TYPES.syntaxerror:
+      __Porffor_printString(__SyntaxError_prototype_toString(arg));
+      return;
+    case Porffor.TYPES.rangeerror:
+      __Porffor_printString(__RangeError_prototype_toString(arg));
+      return;
+    case Porffor.TYPES.evalerror:
+      __Porffor_printString(__EvalError_prototype_toString(arg));
+      return;
+    case Porffor.TYPES.urierror:
+      __Porffor_printString(__URIError_prototype_toString(arg));
+      return;
+    case Porffor.TYPES.test262error:
+      __Porffor_printString(__Test262Error_prototype_toString(arg));
+      return;
   }
 };
 
