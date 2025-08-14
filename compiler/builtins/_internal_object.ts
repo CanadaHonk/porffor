dba8eb5e7c670f7a556830e4073d8022be46f9f1
Oliver Medhurst <honk@goose.icu>
2025-08-14 18:06:59 +0100

    builtins/internal_object: reorder func defs
    test262: 60.79% (+0.02) | 🧪 50594 | 🤠 30756 (+8) | ❌ 7900 (-8) | 💀 11679 | 🏗️ 6 | 💥 232 | ⏰ 21 | 📝 0

--- compiler/builtins/_internal_object.ts
+++ compiler/builtins/_internal_object.ts
@@ -172,15 +172,15 @@ local.set ${obj}`;
     // it does not, make it
     const underlying: object = {};
     if (Porffor.type(_obj) == Porffor.TYPES.function) {
+      __Porffor_object_fastAdd(underlying, 'length', __Porffor_funcLut_length(obj), 0b0010);
+      __Porffor_object_fastAdd(underlying, 'name', __Porffor_funcLut_name(obj), 0b0010);
+
       if (ecma262.IsConstructor(_obj)) { // constructor
         // set prototype and prototype.constructor if function and constructor
         const proto: object = {};
         __Porffor_object_fastAdd(underlying, 'prototype', proto, 0b1000);
         __Porffor_object_fastAdd(proto, 'constructor', _obj, 0b1010);
       }
-
-      __Porffor_object_fastAdd(underlying, 'name', __Porffor_funcLut_name(obj), 0b0010);
-      __Porffor_object_fastAdd(underlying, 'length', __Porffor_funcLut_length(obj), 0b0010);
     }
 
     if (Porffor.type(_obj) == Porffor.TYPES.array) {
