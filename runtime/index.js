7b622a195ab44057b3db035c7dcfb72ee5b217da
Oliver Medhurst <honk@goose.icu>
2025-08-14 23:55:39 +0100

    site: add test262 # marker

--- runtime/index.js
+++ runtime/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import fs from 'node:fs';
-globalThis.version = '0.60.3';
+globalThis.version = '0.60.4';
 
 // deno compat
 if (typeof process === 'undefined' && typeof Deno !== 'undefined') {
