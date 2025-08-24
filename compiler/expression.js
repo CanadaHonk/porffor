648c4d41c871fe2e232be11c341f2ed26118fa3b
Oliver Medhurst <honk@goose.icu>
2025-08-24 19:32:18 +0100

    expression: delete i64
    test262: 60.80% | 🧪 50594 | 🤠 30760 | ❌ 7902 | 💀 11676 | 🏗️ 6 | 💥 232 | ⏰ 18 | 📝 0

--- compiler/expression.js
+++ compiler/expression.js
@@ -46,31 +46,6 @@ export const operatorOpcode = {
     ]
   },
 
-  i64: {
-    '+': Opcodes.i64_add,
-    '-': Opcodes.i64_sub,
-    '*': Opcodes.i64_mul,
-    '/': Opcodes.i64_div_s,
-    '%': Opcodes.i64_rem_s,
-
-    '&': Opcodes.i64_and,
-    '|': Opcodes.i64_or,
-    '^': Opcodes.i64_xor,
-    '<<': Opcodes.i64_shl,
-    '>>': Opcodes.i64_shr_s,
-    '>>>': Opcodes.i64_shr_u,
-
-    '==': Opcodes.i64_eq,
-    '===': Opcodes.i64_eq,
-    '!=': Opcodes.i64_ne,
-    '!==': Opcodes.i64_ne,
-
-    '>': Opcodes.i64_gt_s,
-    '>=': Opcodes.i64_ge_s,
-    '<': Opcodes.i64_lt_s,
-    '<=': Opcodes.i64_le_s
-  },
-
   f64: {
     '+': Opcodes.f64_add,
     '-': Opcodes.f64_sub,
