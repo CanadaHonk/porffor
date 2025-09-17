fc112893771a7441f971b21ff7d70b018423eae0
Oliver Medhurst <honk@goose.icu>
2025-09-17 15:33:55 +0100

    site: add lambda bars, tweaks

--- runtime/index.js
+++ runtime/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import fs from 'node:fs';
-globalThis.version = '0.60.16';
+globalThis.version = '0.60.20';
 
 // deno compat
 if (typeof process === 'undefined' && typeof Deno !== 'undefined') {
