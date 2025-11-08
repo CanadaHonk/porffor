343a18b377fef7accd23f9775483d24bf29587fb
Oliver Medhurst <honk@goose.icu>
2025-11-08 23:12:53 +0000

    precompile: rewrite comptime reset prefs code
    ~3% smaller output file and more readable diffs
    
    test262: 61.72% | 🧪 50718 | 🤠 31304 | ❌ 7603 | 💀 11552 | 🏗️ 0 | 💥 239 | ⏰ 20

--- runtime/index.js
+++ runtime/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import fs from 'node:fs';
-globalThis.version = '0.60.26';
+globalThis.version = '0.60.27';
 
 // deno compat
 if (typeof process === 'undefined' && typeof Deno !== 'undefined') {
