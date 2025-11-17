b768354aa8db1360c55d39c1d132611b33b29455
Oliver Medhurst <honk@goose.icu>
2025-11-17 17:18:43 +0000

    prefs: turn off coctc by default
    it broke getter/setter oops
    
    test262: 61.74% (+0.01) | 🧪 50718 | 🤠 31314 (+6) | ❌ 7594 (-6) | 💀 11551 | 🏗️ 0 | 💥 239 | ⏰ 20

--- compiler/prefs.js
+++ compiler/prefs.js
@@ -1,4 +1,4 @@
-const onByDefault = [ 'treeshakeWasmImports', 'alwaysMemory', 'indirectCalls', 'optUnused', 'data', 'passiveData', 'rmUnusedTypes', 'optTypes', 'coctc', 'ctHash', 'closures' ];
+const onByDefault = [ 'treeshakeWasmImports', 'alwaysMemory', 'indirectCalls', 'optUnused', 'data', 'passiveData', 'rmUnusedTypes', 'optTypes', 'ctHash', 'closures' ];
 
 const nameToKey = x => x.replace(/[a-z]\-[a-z]/g, y => `${y[0]}${y[2].toUpperCase()}`);
 
