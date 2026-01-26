be42c7620bc3fc9390c6a4c04237103534072446
Oliver Medhurst <honk@goose.icu>
2026-01-26 23:11:53 +0000

    ci: move publish from git hook to ci

--- publish.js
+++ publish.js
@@ -6,16 +6,16 @@ const packageVersion = JSON.parse(packageJson).version;
 const majorminor = packageVersion.split('.').slice(0, 2).join('.');
 const patch = parseInt(packageVersion.split('.')[2].split(/[^0-9\-]/)[0]) + 1;
 const version = `${majorminor}.${patch}`;
+
 fs.writeFileSync('package.json', packageJson.replace(`"${packageVersion}"`, `"${version}"`));
 
 const jsrJson = fs.readFileSync('jsr.json', 'utf8');
 fs.writeFileSync('jsr.json', jsrJson.replace(`"${packageVersion}"`, `"${version}"`));
 
-fs.writeFileSync('runtime/index.js', fs.readFileSync('runtime/index.js', 'utf8')
-  .replace(/globalThis\.version = '.*?';/, `globalThis.version = '${version}';`));
+fs.writeFileSync(
+  'runtime/index.js',
+  fs.readFileSync('runtime/index.js', 'utf8')
+    .replace(/globalThis\.version = '.*?';/, `globalThis.version = '${version}';`)
+);
 
-execSync(`git add package.json jsr.json runtime/index.js`, { stdio: 'inherit' });
-execSync(`git commit --amend -C HEAD --no-verify`, { stdio: 'inherit' });
-
-execSync(`${process.env.NPM_BINARY ?? 'npm'} publish`, { stdio: 'inherit' });
-execSync(`${process.env.NPM_BINARY ?? 'npm'} x jsr -- publish --token=${process.env.JSR_TOKEN} --allow-dirty`, { stdio: 'inherit' });
+execSync('node test262/generateHistoricalData.js', { stdio: 'inherit' });
